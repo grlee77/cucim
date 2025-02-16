37e499fd817ef22f9671adf35261ab8a644d5fe5
Gregory Lee <grlee77@gmail.com>
2025-02-16 12:26:09 -0500

    use pdist_mas_blockwise from _shared/distance.py instead

--- python/cucim/src/cucim/skimage/measure/_regionprops_gpu_convex.py
+++ python/cucim/src/cucim/skimage/measure/_regionprops_gpu_convex.py
@@ -3,140 +3,16 @@ import warnings
 from collections.abc import Sequence
 
 import cupy as cp
-import numpy as np
 
+from cucim.skimage._shared.distance import pdist_max_blockwise
 from cucim.skimage._shared.utils import _ndarray_argwhere
 from cucim.skimage._vendored import ndimage as ndi
-
-try:
-    from cupyx.scipy.spatial.distance import cdist
-
-    have_cdist = True
-except ImportError:
-    have_cdist = False
 
 convex_deps = dict()
 convex_deps["image_convex"] = ["image"]  # computed by regionprops_image
 convex_deps["area_convex"] = ["image_convex"]
 convex_deps["feret_diameter_max"] = ["image_convex"]
 convex_deps["solidity"] = ["area", "area_convex"]
-
-
-def pdist_max_blockwise(
-    coords,
-    metric="sqeuclidean",
-    *,
-    return_argmax=False,
-    coords_per_block=4000,
-    cdist_kwargs={},
-):
-    """Find maximum pointwise distance.
-
-    Computes by processing blocks of coordinates to reduce overall memory
-    requirement. The memory used at runtime will be proportional to
-    ``coords_per_block**2``.
-
-    A block size of >= 2000 is recommended to overhead poor GPU resource usage
-    and to reduce kernel launch overhead.
-
-    Parameters
-    ----------
-    coords : np.ndarray (num_points, ndim)
-        The coordinates to process.
-    metric : str, optional
-        Can be any metric supported by `scipy.spatial.distance.cdist`. The
-        default is the squared Euclidean distance (sqeuclidean).
-    return_argmax : bool, optional
-        If True, a tuple containing two indices into the coords array that
-        correspond to the maximum pairwise distance are returned.
-    coords_per_block : bool, optional
-        Internally, calls to cdist will be made with subsets of coords where
-        the subset size is (coords_per_block, ndim).
-    cdist_kwargs = dict, optional
-        Can provide any additional kwargs to cdist (e.g. `p` for Minkowski
-        norms).
-
-    cdist requires num_coords**2 elements. This is very wasteful if we only
-    want to find the maximum pointwise distance. In that case, this function
-    processes smaller blocks of the overall cdist result, keeping a log of the
-    maximum value.
-
-    A schematic of the block sizes processed for an array of 10,000 coordinates
-    with a block size of 4000 points is shown below:
-
-      ┌────────┬────────┬────────┐
-      │  4000  │  4000  │  2000  │
-      │  4000  │  4000  │  2000  │
-      ├────────┼────────┼────────┤
-      │   X    │  4000  │  2000  │
-      │        │  4000  │  2000  │
-      ├────────┼────────┼────────┤
-      │   X    │   X    │  2000  │
-      │        │        │  2000  │
-      └────────┴────────┴────────┘
-
-    where due to the symmetry of the pdist matrix, we don't compute the lower
-    triangular blocks.
-    """
-    num_coords, ndim = coords.shape
-    if num_coords == 0:
-        raise RuntimeError("No coordinates to process")
-
-    blocks_per_dim = math.ceil(num_coords / coords_per_block)
-    if blocks_per_dim > 1:
-        # reuse the same temporary storage array for most blocks
-        # (last block in row and column may be smaller)
-        temp = cp.zeros((coords_per_block, coords_per_block), dtype=cp.float32)
-    if coords.dtype not in [cp.float32, cp.float64]:
-        coords = coords.astype(cp.float32, copy=False)
-    if not coords.flags.c_contiguous:
-        coords = cp.ascontiguousarray(coords)
-    max_dist = 0
-    for i in range(blocks_per_dim):
-        for j in range(blocks_per_dim):
-            if j < i:
-                # skip symmetric regions
-                continue
-            sl_m = slice(
-                i * coords_per_block,
-                min((i + 1) * coords_per_block, num_coords),
-            )
-            sl_n = slice(
-                j * coords_per_block,
-                min((j + 1) * coords_per_block, num_coords),
-            )
-            # print(f"\t{i=}: {sl_m}, {j=}: {sl_n}")
-            coords_block1 = coords[sl_m, :]
-            coords_block2 = coords[sl_n, :]
-            if i < blocks_per_dim - 1 and j < blocks_per_dim - 1:
-                cdist(coords_block1, coords_block2, metric=metric, out=temp)
-                current_max = float(temp.max())
-                if return_argmax:
-                    if current_max > max_dist:
-                        loc_index = i, j
-                        distances_max = temp.copy()
-                        max_dist = current_max
-                else:
-                    max_dist = max(current_max, max_dist)
-            else:
-                out = cdist(coords_block1, coords_block2, metric=metric)
-                current_max = float(out.max())
-                if return_argmax:
-                    if current_max > max_dist:
-                        loc_index = i, j
-                        distances_max = out.copy()
-                        max_dist = current_max
-                else:
-                    max_dist = max(current_max, max_dist)
-    if return_argmax:
-        i, j = loc_index
-        loc = np.unravel_index(int(distances_max.argmax()), distances_max.shape)
-        loc = (
-            int(loc[0]) + i * coords_per_block,
-            int(loc[1]) + j * coords_per_block,
-        )
-        return max_dist, loc
-    return max_dist
 
 
 def regionprops_area_convex(
@@ -210,7 +86,7 @@ def _feret_diameter_max(image_convex, spacing=None, return_argmax=False):
     )
     if return_argmax:
         return math.sqrt(out[0]), out[1]
-    return math.sqrt(out)
+    return math.sqrt(out[0])
 
 
 def regionprops_feret_diameter_max(
